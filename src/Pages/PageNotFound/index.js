7601751e7899c9037ded90104224ecd3cebac460
Victor Cardoso <victorclmatos@gmail.com>
2024-09-29 09:03:31 -0300

    adjusted page not found

--- src/Pages/PageNotFound/index.js
+++ src/Pages/PageNotFound/index.js
@@ -1,8 +1,24 @@
+import { Link } from 'react-router-dom';
 import styles from './PageNotFound.module.css'
 
 function PageNotFound() {
     return(
-        <h1>Page Not Found!!!</h1>
+        <div className={styles.PaginaNaoEncontrada}>
+            <div>
+                <span className={styles.texto404}>404</span>
+                <h1 className={styles.titulo}>
+                    Oops! Page not found!
+                </h1>
+            </div>
+
+            <p className={styles.paragrafo}>
+                Is this what you were looking for? Please verify the address in the adress bar or click the link below to return to the previous page.
+            </p>
+
+            <Link to='/'>
+                <div className={styles.backPage}>Previous Page</div>
+            </Link>
+        </div>
     )
 }
 
