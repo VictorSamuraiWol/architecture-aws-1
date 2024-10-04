da82fa7eeee93133d7fd2686cc1ca53c1503a9b0
Victor Cardoso <victorclmatos@gmail.com>
2024-10-03 22:31:04 -0300

    added continuous timer

--- src/Components/Header/Timer/index.js
+++ src/Components/Header/Timer/index.js
@@ -1,9 +1,9 @@
 import styles from './Timer.module.css';
 import React, { useState, useEffect, useRef } from 'react';
 
-const Timer = () => {
+const Timer = ({nextQuestions}) => {
   const [time, setTime] = useState(120); // 2 minutos = 120 segundos
-  const [isRunning, setIsRunning] = useState(false);
+  const [isRunning, setIsRunning] = useState(true);
   const timerRef = useRef(null);
 
   // Function start timer
@@ -16,16 +16,25 @@ const Timer = () => {
     }
   };
 
-  // Function stop timer
-  const stopTimer = () => {
+  // Function pause timer
+  const pauseTimer = () => {
     setIsRunning(false); // Stop execution state
     clearInterval(timerRef.current); // Clears the range, sttopping the timer
   };
 
+  // Effect to start the timer automatically when loading the page
+  useEffect(() => {
+    timerRef.current = setInterval(() => {
+      setTime(prevTime => prevTime - 1);
+    }, 1000);
+
+    return () => clearInterval(timerRef.current); // Clears the range when disassembling the component
+   }, []);
+
   // Effect to stop the timer when it reaches 0
   useEffect(() => {
     if (time === 0) {
-      stopTimer();
+      pauseTimer();
     }
   }, [time]);
 
@@ -41,7 +50,7 @@ const Timer = () => {
       <h1>{formatTime(time)}</h1>
       <div>
         <button onClick={startTimer} disabled={isRunning || time === 0}>Play</button>
-        <button onClick={stopTimer} disabled={!isRunning}>Pause</button>
+        <button onClick={pauseTimer} disabled={!isRunning}>Pause</button>
       </div>
     </div>
   );
