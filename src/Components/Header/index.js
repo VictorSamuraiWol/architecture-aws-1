badc774cf25861f9b7a7d5748a0ea7e2cb352c30
Victor Cardoso <victorclmatos@gmail.com>
2024-10-03 21:22:12 -0300

    added new resolutions 13-15

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -5,7 +5,7 @@ function Header({ title, newQuestions, nextQuestions }) {
     return(
         <div className={styles.header}>
             <h1>{title}</h1>
-            {(!newQuestions || newQuestions.length === 0) && (!nextQuestions || nextQuestions.length === 0) && <h1>It is necessary to mock an API to view the page contents.</h1>}
+            {(!newQuestions || newQuestions.length === 0) && (!nextQuestions || nextQuestions.length === 0) && <h1>It is necessary to mock an API on port 3001 to view the page contents.</h1>}
         </div>
     )
 
