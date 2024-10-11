a016e70cec674774b6f90f6995224da139250208
Victor Cardoso <victorclmatos@gmail.com>
2024-10-11 14:58:37 -0300

    added mouseon and mouseover events for the styles to the descriptions

--- src/Components/Main/Answer/index.js
+++ src/Components/Main/Answer/index.js
@@ -3,7 +3,6 @@ import styles from './Answer.module.css';
 function Answer({ answer, descriptionP, srcImg, answerDisplay, descriptionDisplay, setDescriptionDisplay, answerMainDisplay, descriptionMainDisplay, setDescriptionMainDisplay }) {
 
     function description() {
-
         if (document.querySelector('#descriptionId').classList.contains(`${styles.visibleDescription}`)) {
                 descriptionDisplay && setDescriptionDisplay(styles.invisible)
                 descriptionMainDisplay && setDescriptionMainDisplay(styles.invisible)
@@ -12,7 +11,18 @@ function Answer({ answer, descriptionP, srcImg, answerDisplay, descriptionDispla
                 descriptionMainDisplay && setDescriptionMainDisplay(styles.visibleDescription)
             }
 
-        }
+    }
+
+    function descriptionOnMouseOver() {
+        const descriptionNewStyle = document.querySelector('#descriptionId')
+        descriptionNewStyle.classList.add(styles.descriptionNewStyleClass)
+    }
+
+    function descriptionOnMouseOut() {
+        const descriptionDefaultStyle = document.querySelector('#descriptionId')
+        descriptionDefaultStyle.classList.remove(styles.descriptionNewStyleClass)
+
+    }
 
     return(
         <section            
@@ -24,15 +34,23 @@ function Answer({ answer, descriptionP, srcImg, answerDisplay, descriptionDispla
                 id='answerTitle'                
                 className={styles.answerTitle}
             >
+
                 <h3>{answer}</h3>
                 <p>Click here for more information</p>
-            </div>           
-            <p 
+
+            </div>
+
+            <p
+                onMouseOver={descriptionOnMouseOver}
+                onMouseOut={descriptionOnMouseOut} 
                 id='descriptionId' 
                 className={`${descriptionDisplay} ${descriptionMainDisplay}`}
             >
+
                 {descriptionP}
+
             </p>
+
         </section>
     )
 }
