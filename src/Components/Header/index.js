0bc8e520b28e8843a94ee484c09c70dbdf6f87ea
Victor Cardoso <victorclmatos@gmail.com>
2025-02-28 09:29:09 -0300

    ajuste aparecer ou desaparecer timer quando tiver conexão ou não com a api, adjust appear ou disappear timer when to have conection or not with api

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,18 +1,16 @@
 import styles from './Header.module.css';
 import Timer from './Timer';
 
-function Header({ title, newQuestions, nextQuestions, multiQuestions }) {
-
-
+function Header({ title, nextQuestions, multiQuestions }) {
 
     return(
         <div className={styles.header}>
             <h1>{title}</h1>
-            {((newQuestions && (Object.values(newQuestions).length > 0) && !nextQuestions) || (nextQuestions && (Object.values(nextQuestions).length > 0) && !newQuestions) || (multiQuestions && !newQuestions)) && 
+            { nextQuestions && 
                 <Timer />
             }
 
-            {(!newQuestions || newQuestions.length === 0) && (!nextQuestions || nextQuestions.length === 0) && (!multiQuestions || multiQuestions.length === 0) && 
+            {(!nextQuestions || nextQuestions.length === 0) && (!nextQuestions || nextQuestions.length === 0) && (!multiQuestions || multiQuestions.length === 0) && 
                 <h1>
                     It is necessary to mock an API on port 3001 to view the page contents.
                 </h1>
