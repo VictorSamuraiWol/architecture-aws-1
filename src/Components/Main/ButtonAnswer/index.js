f69ee01c9584693b7762bafec3097693b76fa06d
Victor Cardoso <victorclmatos@gmail.com>
2025-03-04 09:24:16 -0300

    atualização para as questões serem respondidas apenas uma vez, update the questions will be answered only one time

--- src/Components/Main/ButtonAnswer/index.js
+++ src/Components/Main/ButtonAnswer/index.js
@@ -2,11 +2,14 @@ import styles from './ButtonAnswer.module.css';
 import errorAudio from '../../../audios/errorAudio.mp3';
 import correctAudio from '../../../audios/correctAudio.mp3';
 import { useOutletContext } from 'react-router-dom';
+import { useState } from 'react';
 
 function ButtonAnswer({ answerDisplay, setAnswerDisplay, descriptionDisplay, setDescriptionDisplay, captureValue, optionValidate, optionInvalidate, answer, optionColor, optionColorMulti, nextOptions, multiOptions, captureValueMulti, optNum1, optNum2, optNum3, optNum4, optNum5, randomIndex }) {
 
     // pegando a variável booleana para habilitar ou desabilitar o som usando 'useOutletContext()' da página base
-    const { validateSound } = useOutletContext(); 
+    const { validateSound } = useOutletContext();
+    //Variável para saber se foi ou não respondida a questão
+    const [questionAnswer, setQuestionAnswer] = useState(false);
 
     function display() {  
         const answerId = document.querySelector('#answerId');
@@ -58,90 +61,120 @@ function ButtonAnswer({ answerDisplay, setAnswerDisplay, descriptionDisplay, set
 
     }
 
+    // Validação da página main
     function validateAnswerPageMain() {
         const errorSound = new Audio(errorAudio);
         const correctSound = new Audio(correctAudio);
         const convertObjArray = [Object.values(nextOptions[randomIndex])[optNum1], Object.values(nextOptions[randomIndex])[optNum2], Object.values(nextOptions[randomIndex])[optNum3], Object.values(nextOptions[randomIndex])[optNum4], Object.values(nextOptions[randomIndex])[optNum5]]
 
         // Observação 1: poderia usar a captura do elemento, por exemplo no evento 'onClick' para pegar o valor e depois comparar com a resposta correta, como a seguir: e.target.parentElement.childNodes[1].innerText.includes(`${answer}`) em vez de usar o for para iterar sobre todas as opções, se preferir.
+        if(questionAnswer === true) {
+            // para manter a resposta sempre visível
+            answerDisplay && setAnswerDisplay(styles.visibleAnswer)
+            
+            //alerta avisando para passar para a próxima questão
+            alert('Ops!!! Já foi respondida está questão, por favor, passe para a próxima questão.')
+        } else {
+            for(let i=0; i < 5; i++) {
+                if (nextOptions && `${convertObjArray[i]}`.includes(`${answer}`) && captureValue !== '') {
+                    //added validate class
+                    const correctOption = document.querySelectorAll('.optionNext')[i];
+                    correctOption.classList.remove(optionColor)
+                    correctOption.classList.add(optionValidate)
 
-        for(let i=0; i < 5; i++) {
-            if (nextOptions && `${convertObjArray[i]}`.includes(`${answer}`) && captureValue !== '') {
-                //added validate class
-                const correctOption = document.querySelectorAll('.optionNext')[i];
-                correctOption.classList.remove(optionColor)
-                correctOption.classList.add(optionValidate)
+                    //added invalide option class
+                    if (i !== parseInt(captureValue) && captureValue !== '') {
+                        const wrongOptionNext = document.querySelectorAll('.optionNext')[parseInt(captureValue)];
 
-                //added invalide option class
-                if (i !== parseInt(captureValue) && captureValue !== '') {
-                    const wrongOptionNext = document.querySelectorAll('.optionNext')[parseInt(captureValue)];
+                        wrongOptionNext.classList.remove(optionColor)
+                        // wrongOptionNext.classList.remove(optionValidate)
+                        wrongOptionNext.classList.add(optionInvalidate)
 
-                    wrongOptionNext.classList.remove(optionColor)
-                    // wrongOptionNext.classList.remove(optionValidate)
-                    wrongOptionNext.classList.add(optionInvalidate)
+                        //play error audio
+                        validateSound === true && errorSound.play(); 
 
-                    //play error audio
-                    validateSound === true && errorSound.play(); 
-               
-                } else {
-                    //play correct audio
-                    validateSound === true && correctSound.play();
+                        //questionAnswer se torna true ao responder
+                        setQuestionAnswer(true)
 
-                }
-            } else {}
+                    } else {
+                        //play correct audio
+                        validateSound === true && correctSound.play();
+
+                        //questionAnswer se torna true ao responder
+                        setQuestionAnswer(true)
+
+                    }
+                } else {}
+            }
 
         }
         alertOption()
-        cleanOptions()
+
+        //para a função limpar as opções ser chamada se for a primeira vez que marcou as opções
+        questionAnswer === false && cleanOptions()
 
     }
 
+    // Validação da página multi
     function validateAnswerPageMulti() {
         const errorSound = new Audio(errorAudio);
         const correctSound = new Audio(correctAudio);
         const captureOptionsNextMulti = document.querySelectorAll('.optionNextMulti')      
         const captureOptionsNextMultiInput = document.querySelectorAll('.optionNextMulti input')
         const captureOptionsNextMultiP = document.querySelectorAll('.optionNextMulti p')
+    
+        if(questionAnswer === true) {
+            // para manter a resposta sempre visível
+            answerDisplay && setAnswerDisplay(styles.visibleAnswer)
+            
+            //alerta avisando para passar para a próxima questão
+            alert('Ops!!! Já foi respondida está questão, por favor, passe para a próxima questão.')
+        } else {
+            const checkedValues = [...captureOptionsNextMultiInput]
+                .filter(input => input.checked)
+                .map(input => input.value)
 
-        const checkedValues = [...captureOptionsNextMultiInput]
-            .filter(input => input.checked)
-            .map(input => input.value)
+            const checkedValuesP = [...captureOptionsNextMultiP]
+            for(let i=0; i<checkedValues.length; i++) { 
 
-        const checkedValuesP = [...captureOptionsNextMultiP]
-        for(let i=0; i<checkedValues.length; i++) { 
+                if (checkedValues.length === 2 && checkedValuesP[checkedValues[i]].innerText.includes('true')) {
 
-            if (checkedValues.length === 2 && checkedValuesP[checkedValues[i]].innerText.includes('true')) {
+                    captureOptionsNextMulti[checkedValues[i]].classList.add(optionValidate)
+                    captureOptionsNextMulti[checkedValues[i]].classList.remove(optionColorMulti)
 
-                captureOptionsNextMulti[checkedValues[i]].classList.add(optionValidate)
-                captureOptionsNextMulti[checkedValues[i]].classList.remove(optionColorMulti)
+                    if (checkedValuesP[checkedValues[0]].innerText.includes('true') && checkedValuesP[checkedValues[1]].innerText.includes('true')) {
+                        // som só irá tocar quando estiver tudo correto
+                        validateSound === true && correctSound.play();
 
-                if (checkedValuesP[checkedValues[0]].innerText.includes('true') && checkedValuesP[checkedValues[1]].innerText.includes('true')) {
-                    // som só irá tocar quando estiver tudo correto
-                    validateSound === true && correctSound.play();
-                }
-
-            } else if (checkedValues.length === 2 && checkedValuesP[checkedValues[i]].innerText.includes('true') === false) { 
-
-                captureOptionsNextMulti[checkedValues[i]].classList.add(optionInvalidate)
-                captureOptionsNextMulti[checkedValues[i]].classList.remove(optionColorMulti)
-
-                for(let i=0; i<checkedValuesP.length; i++) {
-                    if (checkedValuesP[i].innerText.includes('true')) {
-                        captureOptionsNextMulti[i].classList.add(optionValidate)
-                        captureOptionsNextMulti[i].classList.remove(optionColorMulti)                    
+                        //questionAnswer se torna true ao responder
+                        setQuestionAnswer(true)
                     }
-                }            
-                validateSound === true && errorSound.play();
 
-            } else {}
+                } else if (checkedValues.length === 2 && checkedValuesP[checkedValues[i]].innerText.includes('true') === false) { 
 
-        }
-        // alerta para marcar as opções quando não tiver nenhuma ou mais do que duas marcadas e ser ativada somente na página multi       
-        if (captureOptionsNextMulti.length > 0 && ((checkedValues.length === 0) || (checkedValues.length < 2) || (checkedValues.length > 2))) {
-            alert('Por favor, marque 2 opções!')
-            clearAnswer()
+                    captureOptionsNextMulti[checkedValues[i]].classList.add(optionInvalidate)
+                    captureOptionsNextMulti[checkedValues[i]].classList.remove(optionColorMulti)
+
+                    for(let i=0; i<checkedValuesP.length; i++) {
+                        if (checkedValuesP[i].innerText.includes('true')) {
+                            captureOptionsNextMulti[i].classList.add(optionValidate)
+                            captureOptionsNextMulti[i].classList.remove(optionColorMulti)                    
+                        }
+                    }            
+                    validateSound === true && errorSound.play();
+                    //questionAnswer se torna true ao responder
+                    setQuestionAnswer(true)
+                } else {}
+
+            }
+            // alerta para marcar as opções quando não tiver nenhuma ou mais do que duas marcadas e ser ativada somente na página multi       
+            if (captureOptionsNextMulti.length > 0 && ((checkedValues.length === 0) || (checkedValues.length < 2) || (checkedValues.length > 2))) {
+                alert('Por favor, marque 2 opções!')
+                clearAnswer()
+            }
         } 
-        cleanOptions()
+        //para a função limpar as opções ser chamada se for a primeira vez que marcou as opções
+        questionAnswer === false && cleanOptions()
    
     }
 
