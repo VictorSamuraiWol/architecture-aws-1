932f60421583be03bfdaa677b0babd67ab32366f
Victor Cardoso <victorclmatos@gmail.com>
2025-03-11 14:07:36 -0300

    adição do atributo numberQuestion no arquivo json/ add attribute numberQuestion in json archive

---
+++ src/Pages/CreateNewQuestionsOptions/index.js
@@ -0,0 +1,11 @@
+import styles from './CreateNewQuestionsOptions.module.css'
+
+function CreateNewQuestionsOptions () {
+    return(
+        <div className={styles.createNewQuestionsOptions}>
+            Creating...
+        </div>
+    )
+}
+
+export default CreateNewQuestionsOptions
